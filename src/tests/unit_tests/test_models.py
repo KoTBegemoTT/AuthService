e3c4638ee8d607e546450ef7e9e2f0d42086ae4d
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-07-21 09:52:06 +0000

    Добавлены тесты для модлей

---
+++ src/tests/unit_tests/test_models.py
@@ -0,0 +1,40 @@
+import pytest
+
+from app.models import User
+
+
+@pytest.mark.parametrize(
+    'pyload',
+    [
+        pytest.param(
+            {'name': 'user_1', 'password': b'password'}, id='user_1'),
+        pytest.param({'name': 'admin', 'password': b'admin'},
+                     id='username_equals_password'),
+        pytest.param({'name': '', 'password': b''},
+                     id='empty_username_and_password'),
+    ]
+)
+def test_user_model(pyload):
+    user = User(**pyload)
+    assert user.name == pyload.get('name')
+    assert user.password == pyload.get('password')
+
+
+@pytest.mark.parametrize(
+    'pyload, error_msg',
+    [
+        pytest.param(
+            {'password': 'password'},
+            'User.__init__() missing 1 required positional argument: \'name\'',
+            id='no_name'),
+        pytest.param(
+            {'name': 'user'},
+            'User.__init__() missing 1 required positional argument: \'password\'',
+            id='no_password'),
+    ]
+)
+def test_user_model_fail(pyload, error_msg):
+    with pytest.raises(TypeError) as ex:
+        User(**pyload)
+
+    assert ex.value.args[0] == error_msg
