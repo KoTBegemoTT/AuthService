e328da5aba2419ddb43fcd2b3d0769ce46ccb580
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-07-26 09:47:01 +0500

    удалён класс в файле тестов

--- src/tests/integration/test_urls.py
+++ src/tests/integration/test_urls.py
@@ -11,30 +11,29 @@ user_password_params = [
 ]
 
 
-class TestAuthService:
-    @pytest.mark.parametrize('username, password', user_password_params)
-    def test_register(self, test_client, username, password):
-        response = test_client.post(
-            '/register', json={'name': username, 'password': password})
+@pytest.mark.parametrize('username, password', user_password_params)
+def test_register(self, test_client, username, password):
+    response = test_client.post(
+        '/register', json={'name': username, 'password': password})
 
-        assert response.status_code == status.HTTP_201_CREATED
-        assert len(users) == 1
-        user = list(users.keys())[0]
+    assert response.status_code == status.HTTP_201_CREATED
+    assert len(users) == 1
+    user = list(users.keys())[0]
 
-        assert user.name == username
-        assert verify_password(password, user.password)
+    assert user.name == username
+    assert verify_password(password, user.password)
 
-    @pytest.mark.parametrize('username, password', user_password_params)
-    def test_login(self, test_client, username, password):
-        test_client.post(
-            '/register', json={'name': username, 'password': password})
+@pytest.mark.parametrize('username, password', user_password_params)
+def test_login(self, test_client, username, password):
+    test_client.post(
+        '/register', json={'name': username, 'password': password})
 
-        response = test_client.post(
-            '/auth', json={'name': username, 'password': password})
+    response = test_client.post(
+        '/auth', json={'name': username, 'password': password})
 
-        assert response.status_code == status.HTTP_201_CREATED
-        token = response.json()['token']
+    assert response.status_code == status.HTTP_201_CREATED
+    token = response.json()['token']
 
-        assert token is not None
-        decoded_token = jwt_decode(token)
-        assert decoded_token['name'] == username
+    assert token is not None
+    decoded_token = jwt_decode(token)
+    assert decoded_token['name'] == username
