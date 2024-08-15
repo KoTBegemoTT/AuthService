391c388709d6db9e44bf76d652b9cb5b92781eb2
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-08-15 11:45:53 +0000

    Добавлены настройки db в settings

--- src/app/config.py
+++ src/app/config.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent
@@ -24,5 +23,13 @@ class Settings(BaseSettings):
     file_encoding: str = 'utf-8'
     file_compression_quality: int = 1
 
+    # Настройки db
+    db_user: str = 'postgres'
+    db_password: str = 'postgres'
+    db_host: str = 'host.docker.internal'
+    db_port: str = '5432'
+    db_table: str = 'credit_card'
+    db_url: str = f'postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_table}'
+    db_echo: bool = True
 
 settings = Settings()
