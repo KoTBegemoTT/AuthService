fd73c04317866a0973dc83edef40fb299e279aac
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-08-16 17:12:41 +0000

    вернул включение кафки

--- src/app/main.py
+++ src/app/main.py
@@ -14,9 +14,9 @@ async def lifespan(app: FastAPI):
     """Настройка при запуске и остановке приложения."""
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    # await producer.start()
+    await producer.start()
     yield
-    # await producer.stop()
+    await producer.stop()
 
 
 app = FastAPI(lifespan=lifespan)
