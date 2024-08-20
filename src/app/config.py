af26a18fb3789b898883bdc9d7bb95824ba6c699
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-08-20 12:27:37 +0500

    Изменён способ получения db_url в settings

--- src/app/config.py
+++ src/app/config.py
@@ -30,8 +30,13 @@ class Settings(BaseSettings):
     db_host: str = 'host.docker.internal'
     db_port: str = '5432'
     db_name: str = 'credit_card'
-    db_url: str = f'postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'  # noqa: E501, WPS221
     db_echo: bool = False
+
+
+    @property
+    def db_url(self) -> str:
+        """Ссылка на БД."""
+        return f'postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}'  # noqa: E501, WPS221
 
 
 settings = Settings()
