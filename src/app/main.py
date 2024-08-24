b608424a5d989d22638b2fd4b75dbfd3b917e041
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-08-24 12:27:44 +0500

    добавлены манифесты для кубера

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from app.auth_service.urls import router as users_router
 from app.external.kafka import producer
@@ -23,6 +23,18 @@ app.include_router(users_router)
 async def root():
     """Стартовая страница."""
     return {'message': 'Hello World'}
+
+
+@app.get('/ready', status_code=status.HTTP_200_OK)
+async def ready_check():
+    """Проверка состояния сервиса."""
+    return {'message': 'Service is ready'}
+
+
+@app.get('/live', status_code=status.HTTP_200_OK)
+async def live_check():
+    """Проверка состояния сервиса."""
+    return {'message': 'Service is live'}
 
 
 if __name__ == '__main__':
